1272f2f3d16b78d2da1d73b28be12c6578916ceb
kovalikovalik <kovalikovalik@yandex.ru>
2022-10-09 19:16:26 +0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error ok = new Error(true, 1, "correct");
+        ok.printInfo();
+    }
+
+    public void printInfo() {
+        System.out.println("Активный: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+}
