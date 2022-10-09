6bb43f279e0542161bf564c603b2885457e69b24
kovalikovalik <kovalikovalik@yandex.ru>
2022-10-09 20:28:06 +0500

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -21,10 +21,12 @@ public class Cat {
     public static void main(String[] args) {
         System.out.println("There are gav's food.");
         Cat gav = new Cat();
+        gav.giveNick("Gav");
         gav.eat("kotleta");
         gav.show();
         System.out.println("There are black's food.");
         Cat black = new Cat();
+        black.giveNick("Black");
         black.eat("fish");
         black.show();
     }
