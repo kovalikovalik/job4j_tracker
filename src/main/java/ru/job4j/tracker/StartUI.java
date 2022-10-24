c781bca6ce9126ec32e2d2a382ccbf420f69f2b0
kovalikovalik <kovalikovalik@yandex.ru>
2022-10-24 15:59:31 +0500

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,7 +6,6 @@ public class StartUI {
 
     public static void main(String[] args) {
         Item item = new Item();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-            System.out.println(formatter.format(item.getCreated()));
+            System.out.println(item);
     }
 }
