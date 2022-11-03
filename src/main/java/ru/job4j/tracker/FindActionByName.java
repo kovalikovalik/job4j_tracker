af57464b2e41d404a157c8c2ed9ceab4d802eeb6
kovalikovalik <kovalikovalik@yandex.ru>
2022-11-03 14:18:06 +0500

    8. Реализация меню за счет шаблона стратегия. [#181780]

---
+++ src/main/java/ru/job4j/tracker/FindActionByName.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+public class FindActionByName implements UserAction {
+    @Override
+    public String name() {
+        return "Find item by name";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Find item by name ===");
+        String name = input.askStr("Enter name: ");
+        Item[] items = tracker.findByName(name);
+        if (items.length > 0) {
+            for (Item item : items) {
+                System.out.println(name);
+            }
+        } else {
+            System.out.println("Заявки с именем: " + name + " не найдены.");
+        }
+
+        return true;
+    }
+}
