44bf29f5469dd6189056291d5f0e85f218d1330f
kovalikovalik <kovalikovalik@yandex.ru>
2022-11-18 11:47:12 +0500

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,37 @@
+package ru.job4j.ooa;
+
+public final class Airbus extends Aircraft {
+
+    private static int countEngine = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        countEngine = name.equals("A380") ? 4 : countEngine;
+        System.out.println("Количество двигателей равно: " + countEngine);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
